2062be6e1c7eb65523ea56239557c1de3477f5b4
Peter Badson <danny41wizard@gmail.com>
2020-01-21 02:27:21 +0300

    Fixed some directory names troubles (I hope)

---
+++ Specialities/routes.py
@@ -0,0 +1,3 @@
+from flask import Blueprint
+
+specializations = Blueprint('specializations', __name__, template_folder='templates')
---
+++ faculties/routes.py
@@ -0,0 +1,3 @@
+from flask import Blueprint
+
+faculties = Blueprint('faculties', __name__, template_folder='templates')

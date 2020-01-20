1e2957b0566c3d58fd0c25627dc93f0ad70e1080
Peter Badson <danny41wizard@gmail.com>
2020-01-21 01:32:12 +0300

    Added constructors to models classes

--- main/models.py
+++ main/models.py
@@ -8,6 +8,9 @@ class Faculty(db.Model):
     def __repr__(self):
         return f'<Faculty: {self.title}>'
 
+    def __init__(self, title):
+        self.title = title
+
 
 
 class Specialty(db.Model):
@@ -19,6 +22,10 @@ class Specialty(db.Model):
     def __repr__(self):
         return f'<Speicality: {self.faculty} {self.title}>'
 
+    def __init__(self, title, faculty_id):
+        self.title = title
+        self.faculty_id = faculty_id
+
 
 class Exam(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -26,6 +33,9 @@ class Exam(db.Model):
 
     def __repr__(self):
         return f'<Exam: {self.title}>'
+
+    def __init__(self, title):
+        self.title = title
 
 
 class Enrolee(db.Model):
@@ -52,6 +62,23 @@ class Enrolee(db.Model):
     exam3_score = db.Column(db.Integer)
 
     total_scores = db.Column(db.Integer)
+
+    def __init__(self, first_name, last_name, age, phone_number, speciality_id, \
+        exam1_id, exam2_id, exam3_id, exam1_score, exam2_score, exam3_score):
+            self.first_name = first_name
+            self.last_name = last_name
+            self.age = age
+            self.phone_number = phone_number
+            self.speciality_id = speciality_id
+            self.exam1_id = exam1_id
+            self.exam2_id = exam2_id
+            self.exam3_id = exam3_id
+            self.exam1_score = exam1_score
+            self.exam2_score = exam2_score
+            self.exam3_score = exam3_score
+
+            self.total_scores = exam1_score + exam2_score + exam3_score
+        
 
 
     def __repr__(self):
